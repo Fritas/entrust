3e04ac2dd6da53faf1feee13ab9be01386119a73
Fritas <adamascenosj@gmail.com>
2018-09-05 08:56:07 -0300

    Update: buttons para mobile

--- app/control.py
+++ app/control.py
@@ -67,4 +67,4 @@ def page_not_found(e):
      mensagem='O URL informado não condiz com nenhuma das nossas páginas. Por favor retorne a página inicial!')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="192.168.56.1")
