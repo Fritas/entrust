919685e11d3a6584f175956cea884954b0da4891
Fritas <adamascenosj@gmail.com>
2018-09-20 23:03:17 -0300

    Update

--- app/teste.py
+++
@@ -1,8 +0,0 @@
-entrada = 1
-while entrada != 0:
-    try:
-        entrada = int(input("Entre com um número: "))
-    except ValueError as error:
-        print(error)
-    else:
-        print("É um número: ", entrada)
