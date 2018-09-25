0e2d65ddac2de2e53afc0c9c615c56c8dd92b97e
Fritas <adamascenosj@gmail.com>
2018-09-24 23:08:40 -0300

    Update

--- app/control.py
+++ app/control.py
@@ -25,46 +25,47 @@ def graph_function():
     # se nao ter o que requisitar eh recebido None
     try:
         input_function = request.args.get('input_function')
+        if not input_function:
+            raise Exception("Função vazia!")
     except Exception as identifier:
-        print("\n\nControl Error: ", identifier)
+        print("\n\nControl Error: %s \n" %(identifier))
     else:
-        if input_function:
-            dic_page['input_function'] = input_function
+        dic_page['input_function'] = input_function
+        try:
+            compiler = Compiler(input_function)
+            if not compiler.valid_function():
+                raise InvalidCompiler()
+        except InvalidCompiler as identifier:
+            print("\n\n Control error: %s \n" %(identifier))
+            dic_page['error'] = compiler.error
+        except Exception as identifier:
+            print("\n\n Control error: %s \n" %(identifier))
+        else:
+            #requisitar coeficientes via get, caso eles ja estejam expressos pelo usuario anteriormente
+            for coefficient in compiler.dic_coefficients:
+                valor = request.args.get(coefficient)
+                if valor:
+                    compiler.dic_coefficients[coefficient] = valor
+            dic_page['coefficients'] = compiler.dic_coefficients
+            print("Dic: ", compiler.dic_coefficients)
+            #concatenar a string para o grafico
+            dic_page['function_graph'] = (compiler.compiler_function_web() %(compiler.dic_coefficients))
+            dic_page['function_math'] = (compiler.compiler_string_math() %(compiler.dic_coefficients))
+            #criar funcao
             try:
-                compiler = Compiler(input_function)
-                if not compiler.valid:
-                    raise InvalidCompiler
-            except InvalidCompiler as identifier:
-                print("\nControl Error: ", identifier)
-                dic_page['error'] = compiler.error
-            except Exception as identifier:
-                print("\n\nControl Error: ", identifier)
-            else:
-                #concatenar a string para o grafico 
-                dic_page['function_graph'] = (compiler.compiled_string_web %(compiler.dic_coefficients))
-                dic_page['function_math'] = (compiler.compiled_string_math %(compiler.dic_coefficients))
-                #requisitar variveis via get, caso eles ja estejam expressos pelo usuario anteriormente
-                for coefficient in compiler.dic_coefficients:
-                    valor = request.args.get(coefficient)
-                    if valor:
-                        compiler.dic_coefficients[coefficient] = valor
-                #criar funcao
-                try:
-                    function = Function(compiler.compiled_string_python,
-                                                    compiler.dic_coefficients)
-                    print(function.solve_real)
-                except Exception as identifier:
-                    print('\n\nControl Error: ', identifier)
-                else: #realizar caso não aja erros na criacao da function
-                    if function.solve_real.is_EmptySet:
-                        dic_page['solve_real'] = {}
-                    else:
-                        dic_page['solve_real'] = function.solve_real
-            finally:
-                dic_page['coefficients'] = compiler.dic_coefficients
-    finally: #em todos os casos deve ser renderizado a pagina function
-        return render_template('graph_function.html', dic=dic_page)
+                function = Function(compiler.compiler_function_python(), compiler.dic_coefficients)
 
+            except Exception as identifier:
+                print("\n\n Control error: %s \n" %(identifier))
+            else:
+                if function.solve_real.is_EmptySet:
+                    dic_page['solve_real'] = {}
+                else:
+                    dic_page['solve_real'] = function.solve_real
+                print("Solve real: ", dic_page['solve_real'])
+    finally:
+        return render_template('graph_function.html', dic=dic_page)
+           
 @app.route('/linear_system', methods=['GET', 'POST'])
 def graph_linear_system():
     return render_template('graph_linear_system.html')
