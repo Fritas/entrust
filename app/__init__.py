94dc694d3978cdf483777d5dc7ea8dfd928a1185
Adriano Damasceno da Silva Júnior <adamascenosj@gmail.com>
2018-10-31 10:40:15 -0300

    Update: add modulo

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+"""
+    __init__ do módulo app
+"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_manager import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+#app
+app = Flask(__name__)
+
+#database
+db = SQLAlchemy
+migrate = Migrate(app, db)
+
+#manager
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+#controllers
+from app.controllers import default
+from app.controllers import error
