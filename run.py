cbced740202bce2bf20a6c187ee0e6ca344f7e3b
Adriano Damasceno da Silva Júnior <adamascenosj@gmail.com>
2018-11-02 09:57:40 -0300

    Update: add run file

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from socket import gethostbyname, gethostname
-from app import app
+from app import manager
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host=gethostbyname(gethostname()))
+    #app.run(debug=True, host=gethostbyname(gethostname()))
     #gethostbyname(gethostname()) > pega o IP da maquina local
+    manager.run()
