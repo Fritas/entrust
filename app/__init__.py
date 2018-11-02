88ddcba3cffa11152376a748438820e25e52a3dc
Adriano Damasceno da Silva Júnior <adamascenosj@gmail.com>
2018-11-02 09:58:29 -0300

    Update: add module file

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@
 """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_manager import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
+from socket import gethostbyname, gethostname
 
 #app
 app = Flask(__name__)
@@ -13,9 +14,18 @@ app = Flask(__name__)
 db = SQLAlchemy
 migrate = Migrate(app, db)
 
+#server
+server = Server(host='192.168.56.1', port=5000, use_debugger=True)
+print('IP: ', gethostbyname(gethostname()))
+
 #manager
-#manager = Manager(app)
-#manager.add_command('db', MigrateCommand)
+manager = Manager(app)
+
+#manager add command
+manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server())
+
+
 
 #controllers
 from app.controllers import default
